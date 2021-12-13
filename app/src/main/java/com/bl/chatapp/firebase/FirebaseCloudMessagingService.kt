d9e89834912ae2f924cc90ef17cb368282989186
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-12-13 13:58:06 +0530

    fixed custom push notification for image message

--- app/src/main/java/com/bl/chatapp/firebase/FirebaseCloudMessagingService.kt
+++ app/src/main/java/com/bl/chatapp/firebase/FirebaseCloudMessagingService.kt
@@ -1,16 +1,17 @@
 package com.bl.chatapp.firebase
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.bl.chatapp.R
 import com.bl.chatapp.common.SharedPref
 import com.bl.chatapp.ui.authentication.AuthenticationActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -43,20 +44,11 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
         val message = remoteMessage.notification?.body ?: ""
         val title = remoteMessage.notification?.title ?: ""
         val image = remoteMessage.notification?.imageUrl ?: ""
-
-        val userId = SharedPref.getInstance(this).getUserId()
-        val intent = Intent(this, AuthenticationActivity::class.java)
-
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
-            .setSmallIcon(R.drawable.whatsapp_logo)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .setContentText(message)
-            .setContentTitle(title)
-            .build()
+        val notification = if(message.isBlank()) {
+            generateImageNotification(title, image.toString())
+        } else {
+            generateMessageNotification(title, message)
+        }
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -66,5 +58,43 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
         notificationManager.createNotificationChannel(notificationChannel)
         notificationManager.notify(Random.nextInt(), notification)
 
+    }
+
+    private fun generateMessageNotification(title: String, message: String): Notification {
+        val intent = Intent(this, AuthenticationActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        return NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_whatsapp_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setContentText(message)
+            .setContentTitle(title)
+            .build()
+    }
+
+    private fun generateImageNotification(title: String, image: String): Notification {
+        val intent = Intent(this, AuthenticationActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val glideBitMap = Glide.with(this).asBitmap().load(image).submit()
+        val bitMapImage = glideBitMap.get()
+        val notificationBigPicture = NotificationCompat.BigPictureStyle().bigPicture(bitMapImage)
+
+        return NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_whatsapp_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setContentTitle(title)
+            .setStyle(notificationBigPicture)
+            .build()
     }
 }
